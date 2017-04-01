2039c355855f797943be7c7bf329241f77c48069
Pranith Macha <pranith@machaiswho.im>
2017-04-01 16:09:31 -0500

    Adding allowed hosts

--- fifa/settings.py
+++ fifa/settings.py
@@ -93,5 +93,6 @@ if ENV == "PROD":
     print("setting prod settings...")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     DEBUG = os.environ.get("DEBUG")
+    ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
     print("setting DEBUG to {0}".format(DEBUG))
 
