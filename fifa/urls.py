09a9308215d9992e0cfd431a0670e9fa0c28b77b
Pranith Macha <pranith@machaiswho.im>
2017-04-01 16:44:57 -0500

    Adding home page

--- fifa/urls.py
+++ fifa/urls.py
@@ -1,10 +1,6 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from fifa import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'fifa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
 ]
