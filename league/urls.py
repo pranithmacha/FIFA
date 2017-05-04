097b10b4aac12425effd4a5c8d27eac9a509135e
sanjana17 <navyasanjana4@gmail.com>
2017-05-03 22:06:23 -0400

    tournament dialog changes

--- league/urls.py
+++ league/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from league import views
+from django.views.generic.base import  TemplateView
 
 urlpatterns = [
     url(r'^register', views.register, name='register'),
     url(r'^create_user', views.create_user, name='create_user'),
     url(r'^create_tournament', views.create_tournament, name='create_tournament'),
-    url(r'^get_tournament_summary/(?P<tournament_id>[0-9])/$', views.get_tournament_summary,
+    url(r'^get_tournament_summary/(?P<tournament_id>[0-9])/', views.get_tournament_summary,
         name='get_tournament_summary'),
+    url(r'^dialog1', TemplateView.as_view(template_name="dialog1.html"), name='dialog1'),
 ]
--- staticfiles/js/tournament.js
+++ staticfiles/js/tournament.js
@@ -50,20 +50,32 @@ debugger;
 	//dialog box
 	$scope.showPrompt = function(ev) {
     // Appending dialog to document.body to cover sidenav in docs app
-    var confirm = $mdDialog.prompt()
-      .title('What would you name your dog?')
-      .textContent('Bowser is a common name.')
-      .placeholder('Dog name')
-      .ariaLabel('Dog name')
-      .initialValue('Buddy')
-      .targetEvent(ev)
-      .ok('Okay!')
-      .cancel('I\'m a cat person');
-
-    $mdDialog.show(confirm).then(function(result) {
-      $scope.status = 'You decided to name your dog ' + result + '.';
+   $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '/dialog1.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
     }, function() {
-      $scope.status = 'You didn\'t name your dog.';
+      $scope.status = 'You cancelled the dialog.';
     });
   };
-})
+});
+
+function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
