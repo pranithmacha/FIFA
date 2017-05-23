e184d74b951f5dd2086ceedb93a850246a2244f5
macha <pranith macha>
2017-05-22 20:26:49 -0500

    Adding whitenoise to requirements

--- fifa/settings.py
+++ fifa/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +73,7 @@ USE_TZ = True
 # STATIC FILES
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
